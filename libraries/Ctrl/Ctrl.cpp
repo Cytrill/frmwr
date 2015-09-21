992592bd5b406ce54d87365a73cdeb99a3d52db2
misterdanb <danb@hasi.it>
2015-09-22 01:08:09 +0200

    Add initial files for ctrl arduino library

---
+++ libraries/Ctrl/Ctrl.cpp
@@ -0,0 +1,89 @@
+#include "Ctrl.h"
+
+Ctrl::Ctrl()
+    : _buttonCallback(NULL),
+      _buttons(0),
+      _leds{ 0, }
+{
+}
+
+Ctrl::~Ctrl()
+{
+}
+
+void Ctrl::begin()
+{
+    // SPI
+    SPI.begin ();  
+
+    // reset the pin mode of MISO, we use it as input
+    SPI.setBitOrder (MSBFIRST);
+    SPI.setDataMode (0);
+    SPI.setFrequency (10000000);
+
+    SPI.writePattern (_leds, sizeof (_leds), 0x00);
+
+    // relevant for boot mode, needs pullup on startup for flash boot
+    pinMode (0, INPUT_PULLUP);
+
+    // UART0 TX. Use for serial tx. Also for the builtin LED.
+    // pinMode (1, SPECIAL);
+    //
+    // UART1 TX during flash programming, no external pullup!
+    pinMode (2, INPUT_PULLUP);
+
+    // UART0 RX. Use for serial rx
+    // pinMode (3, SPECIAL);
+
+    pinMode (4, INPUT_PULLUP);
+    pinMode (5, INPUT_PULLUP);
+
+    // 6:
+
+    // this overrides SPI MISO which we don't use
+    pinMode (12, INPUT_PULLUP);
+
+    // 13: SPI MOSI, we use it for the LEDs, might possibly be used for a button with series resistor as well.
+    // 14: SPI CLK, we use it for the LEDs
+
+    // relevant for boot mode, needs Pulldown on startup
+    pinMode (15, INPUT_PULLUP);
+}
+
+void Ctrl::end()
+{
+    SPI.end();
+}
+
+void Ctrl::registerButtonCallback(void (*callback)(int, bool))
+{
+    _buttonCallback = callback;
+}
+
+bool Ctrl::getButton(int button)
+{
+    return bool(_buttons & button);
+}
+
+void Ctrl::setLed(int led, byte r, byte g, byte b)
+{
+    if (led < NUM_LEDS)
+    {
+        _leds[led * 3] = r;
+        _leds[led * 3 + 1] = g;
+        _leds[led * 3 + 2] = b;
+    }
+
+    SPI.writeBytes(_leds, sizeof(_leds));
+}
+
+void Ctrl::setLeds(byte leds[])
+{
+    memcpy(_leds, leds, NUM_LEDS);
+
+    SPI.writeBytes(_leds, sizeof(_leds));
+}
+
+void Ctrl::loop()
+{
+}
---
+++ libraries/Ctrl/Ctrl.h
@@ -0,0 +1,67 @@
+/*
+ Ctrl.h - Development library for the Cytrill game controller.
+  danb
+  http://hasi.it
+*/
+
+#ifndef CTRL_H
+#define CTRL_H
+
+#include <Arduino.h>
+#include <stdint.h>
+#include "SPI.h"
+#include "esp8266_peri.h"
+
+#define NUM_LEDS 4
+
+/********************************************
+ ***     ____________________________     ***
+ ***    /    _                  _    \    ***
+ ***   /  _ |U| _   L0  L1   _ |X| _  \   ***
+ ***   | |L| _ |R|          |Y| _ |A| |   ***
+ ***   \    |D|                |B|    /   ***
+ ***    \____________________________/    ***
+ ***                                      ***
+ ********************************************/
+
+#define BTN_UP    0x01
+#define BTN_RIGHT 0x02
+#define BTN_DOWN  0x04
+#define BTN_LEFT  0x08
+
+#define BTN_X     0x10
+#define BTN_A     0x20
+#define BTN_B     0x40
+#define BTN_Y     0x80
+
+#define LED_0     0
+#define LED_1     1
+#define LED_2     2
+#define LED_3     3
+
+class Ctrl
+{
+public:
+    Ctrl();
+    ~Ctrl();
+
+    void begin();
+    void end();
+
+    void registerButtonCallback(void (*)(int, bool));
+
+    bool getButton(int);
+
+    void setLed(int, byte, byte, byte);
+    void setLeds(byte[]);
+
+    void loop();
+
+private:
+    void (*_buttonCallback)(int, bool);
+
+    unsigned int _buttons;
+    byte _leds[NUM_LEDS * 3];
+};
+
+#endif
